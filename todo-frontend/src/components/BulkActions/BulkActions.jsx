d61dd373322d0164d4cebf609eeb015a5c571524
KhacKiet <kiethuynh080220@gmail.com>
2025-12-21 16:37:15 +0700

    style: add style for BulkActions

--- todo-frontend/src/components/BulkActions/BulkActions.jsx
+++ todo-frontend/src/components/BulkActions/BulkActions.jsx
@@ -5,20 +5,24 @@ import styles from "./BulkActions.module.scss";
 const cx = classNames.bind(styles);
 
 function BulkActions({ children, selectedIds, handleSelectAll, filteredTodos }) {
+    const isAllSelected = filteredTodos.length > 0 && selectedIds.length === filteredTodos.length;
+
     return (
         <div className={cx("bulk-actions-box")}>
-            <input
-                type="checkbox"
-                checked={selectedIds.length === filteredTodos.length && selectedIds.length > 0}
-                onChange={(e) => handleSelectAll(e)}
-            />
-
-            {selectedIds.length > 0 && (
-                <>
-                    <span>Selected: {selectedIds.length}</span>
-                    {children}
-                </>
-            )}
+            <label className={cx("select-label", { active: isAllSelected })}>
+                <input
+                    hidden
+                    className={cx("checkbox")}
+                    type="checkbox"
+                    checked={selectedIds.length === filteredTodos.length && selectedIds.length > 0}
+                    onChange={(e) => handleSelectAll(e)}
+                />
+                Select All
+            </label>
+            <span className={cx("select-number", { active: selectedIds.length > 0 })}>
+                Selected: {selectedIds.length}
+            </span>
+            <span className={cx("bulk-actions", { active: selectedIds.length > 0 })}>{children}</span>
         </div>
     );
 }
